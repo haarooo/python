e06efd0761699bee2916e1600d7879cd7782013e
haaroooo <pshb200137@gmail.com>
2026-04-15 18:06:00 +0900

    0415 파이썬 1일차

---
+++ day01/hello.py
@@ -0,0 +1,22 @@
+# p.56
+# vscode에서 python 코드 실행하기 위한 확장 프로그램 설치
+print('hello python');
+
+print("hello"*3);
+
+# 인터프리터란? 파이썬 코드를 실행할 수 있는 도구(한줄씩) vs 컴파일(c언어) (파일 단위 번역)
+# 표현식,식별자,키워드,연산자 등 -> 문장 -> 프로그램
+
+import keyword
+print(keyword.kwlist)
+
+# 식별자 : 이름을 붙인 단어 , 변수명/함수명/클래스명 등등
+
+# 76 출력 print()
+print('# 하나만 출럭')
+print()
+
+print(52 , 21 , "hello")
+print('안' , '녕' , '하' , '세' , '요')
+
+print(type("안녕"))
